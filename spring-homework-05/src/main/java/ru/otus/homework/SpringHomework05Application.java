1589b6cba58a74d013cd6fbea334c9015b8cd827
Tatyana Solomatina <Solomatina.T.An@sberbank.ru>
2021-04-02 11:00:37 +0300

    spring-homework-05: new project

---
+++ spring-homework-05/src/main/java/ru/otus/homework/SpringHomework05Application.java
@@ -0,0 +1,13 @@
+package ru.otus.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHomework05Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHomework05Application.class, args);
+    }
+
+}
---
+++ spring-homework-05/src/main/java/ru/otus/homework/domain/Book.java
@@ -0,0 +1,4 @@
+package ru.otus.homework.domain;
+
+public class Book {
+}
